b6ace77987d35f5a051465cdc37453d928514378
ramkishornh3 <ramkishor.sikarwar@gmail.com>
2023-12-18 16:42:17 +0530

    welcome first commit.

---
+++ src/main/java/com/example/aws/demo/RamkishorAwsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamkishorAwsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamkishorAwsDemoApplication.class, args);
+	}
+
+}
